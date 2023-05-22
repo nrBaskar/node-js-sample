99aaa549654f405d4f17e1347b57a46a876516a1
nrBaskar <106078300+nrBaskar@users.noreply.github.com>
2023-05-22 11:42:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World! docker pull is removed')
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost , docker pull is removed:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'))
 })
